6d6a77580915101f7a6b085ec2ea454f39dd8e9b
MarIniOnz <martininiguezdeo@hotmail.com>
2021-07-10 15:09:18 +0200

    push it all

--- agents/agent_Monte_Carlo/montecarlo.py
+++ agents/agent_Monte_Carlo/montecarlo.py
@@ -4,26 +4,88 @@ from agents.agents_random.random import generate_move_random
 
 
 class TreeNode:
+    """
+    Tree structure class to store all nodes and their corresponding values.
+
+    ...
+
+    Attributes
+    ----------
+    board : np.array
+        state of the board (matrix)
+    move: PlayerAction
+        move performed on the board in that turn
+    parent: TreeNode or None
+        parent node in the hierarchy
+    child: TreeNode or None
+        child nodes in the hierarchy
+    turn_player: BoardPiece
+        player who played this turn (last move on board).
+    total_games: int
+        number of games in which this node or its children have performed a random simulation (default is 0)
+    wins: int
+        number of wins in which this node or its children have won in the random simulation (default is 0)
+    winner: bool
+        whether this node is a winning node (winning combination of pieces already in the board) (default is False)
+    loser: bool
+        whether this node is a losing node (losing combination of pieces already in the board) (default is False)
+    terminal: bool
+        whether this is a terminal node (losing/winning/draw) (default is False)
+    is_root: bool
+        whether this node is the is_root of this stage of the game
+    Methods
+    -------
+    new_child(children, main_player, last_action)
+        Appending new children to the tree, using self as parent
+    """
 
     def __init__(self, board, move, parent, turn_player):
-        """ Initialization of the Tree assigning the value to the node."""
+        """ Initialization of the Tree assigning the attributes to the node.
+
+        Parameters
+        ----------
+        board : np.array
+            State of the board (matrix)
+        parent: TreeNode or None
+            Parent node in the hierarchy
+        move: PlayerAction
+            Move performed on the board in that turn
+        turn_player: BoardPiece
+            Player who played this turn (last move on board)
+        """
+
         self.board = board
         self.parent = parent
-        self.child = None
+        self.turn_player = turn_player
         self.move = move
+
+        self.child = None
         self.total_games = 0
         self.wins = 0
         self.winner = False
         self.loser = False
         self.terminal = False
-        self.turn_player = turn_player
+        self.is_root = False
 
-    def new_child(self, children, main_player, last_action):
+    def new_child(self, children, main_player):
+        """Appending new children to the tree, using self as the parent.
+
+        Parameters
+        ----------
+        children: TreeNode
+            new TreeNode structure to be appended as a child to self
+        main_player: BoardPiece
+            piece of the player using this tree node (machine_player)
+        """
+        # Appending the new child and choosing it as node variable
         if self.child is None:
             self.child = []
-        self.child.append(children)  # Append not working
+        self.child.append(children)
         node = self.child[-1]
-        result = check_end_state(node.board, node.turn_player, last_action)
+
+        # Checking whether it is a terminal node (result different from
+        # GameState.STILL_PLAYING and if so, load it into its attributes
+        result = check_end_state(node.board, node.turn_player, node.move)
 
         if main_player == node.turn_player and result == GameState.IS_WIN:
             node.winner = True
@@ -35,14 +97,53 @@ class TreeNode:
             node.terminal = True
 
     def find_ucb1(self):
+        """Finding the node's upper confidence bound (UBC1).
+
+        Returns
+        -------
+        ucb1: None or float
+            if a game has been played by the node already, return the ucb1 value, otherwise return None
+        """
+        ucb1 = None
+
         if self.total_games == 0:
-            return None
+            return ucb1
         else:
+            # sqrt(2) used as exploration parameter
             ucb1 = self.wins / self.total_games + np.sqrt(2 * np.log(self.parent.total_games) / self.total_games)
             return ucb1
 
-    def select_node(self):
+    def find_best_child(self):
+        """Finding the child of a node with best UCB value.
 
+        Returns
+        -------
+        node: TreeNode
+            leaf node with highest UCB values
+        """
+        node = self
+        ucb_values = [children.find_ucb1() for children in node.child]
+
+        # Choosing random children if there is a child which has not been
+        # explored yet (meaning it has None as its UCB value)
+        if None in ucb_values:
+            node = node.child[np.random.randint(len(node.child))]
+        else:
+            node = node.child[np.argmax(ucb_values)]
+
+        return node
+
+    def select_node(self):
+        """Performing the SELECTION step.
+
+        Finding the last node that is not terminal and has no children yet (leaf node),
+        going through a path of selection guided by the UCB values of the nodes.
+
+        Returns
+        -------
+        node: TreeNode
+            leaf node with highest parental UCB values
+        """
         node = self
 
         while node.child is not None and not node.terminal:
@@ -51,43 +152,54 @@ class TreeNode:
 
         return node
 
-    def find_best_child(self):
-
-        node = self
-
-        ucb_values = [children.find_ucb1() for children in node.child]
-        if None in ucb_values:
-            node = node.child[np.random.randint(len(node.child))]
-        else:
-            node = node.child[np.argmax(ucb_values)]
-
-        return node
-
     def check_winning_children(self):
+        """Check whether any children node has a board winning combination.
 
+        Returns
+        -------
+        winners: list
+            indexes of the winners which have a winning combination
+        """
         booleans = np.empty(len(self.child))
         for i, children in enumerate(self.child):
             booleans[i] = children.winner
         winners = np.argwhere(booleans)
 
+        # Return an empty list if no winners among the children
         if len(winners) == 0:
             return []
         else:
             return winners
 
     def check_losing_children(self):
+        """Check whether any children node has a board losing combination.
+
+        Returns
+        -------
+        losers: list
+            indexes of the winners which have a losing combination
+        """
 
         booleans = np.empty(len(self.child))
         for i, children in enumerate(self.child):
             booleans[i] = children.loser
         losers = np.argwhere(booleans)
 
+        # Return an empty list if no losers among the children
         if len(losers) == 0:
             return []
         else:
             return losers
 
-    def expansion_and_back_prop(self, main_player):
+    def opponent_choice(self, action):
+
+        moves = [children.move for children in self.child]
+        ind_opponent_move = int(np.argwhere(moves == action))
+        child_opponent = self.child[ind_opponent_move]
+
+        return child_opponent
+
+    def expansion(self, main_player):
 
         cols = valid_columns(self.board)
         win = False
@@ -98,14 +210,15 @@ class TreeNode:
             if cols.size > 1:
                 for i, column in enumerate(cols):
                     board = old_board.copy()
+
                     apply_player_action(board, column, player=change_player(self.turn_player))
                     self.new_child(TreeNode(board, PlayerAction(column), parent=self, turn_player=change_player(
-                                   self.turn_player)), main_player, column)
+                                   self.turn_player)), main_player)
             else:
                 board = old_board.copy()
                 apply_player_action(board, PlayerAction(cols), player=change_player(self.turn_player))
-                self.new_child(TreeNode(board, cols, parent=self, turn_player=change_player(self.turn_player)),
-                               main_player, cols)
+                self.new_child(TreeNode(board, PlayerAction(cols), parent=self, turn_player=change_player(
+                               self.turn_player)), main_player)
 
             winning_nodes = self.check_winning_children()
             losing_nodes = self.check_losing_children()
@@ -141,29 +254,19 @@ class TreeNode:
         elif not self.terminal and cols is None:
             self.terminal = True
 
-        node.back_prop(win)
+        return node, win
 
-        return node
 
-    def back_prop(self, winning):
+def back_prop(node, winning):
 
-        parent = self
+    parent = node
 
-        while parent is not None:
-            parent.total_games += 1
+    while parent.is_root is False:
+        parent = parent.parent
+        parent.total_games += 1
 
-            if winning:
-                parent.wins += 1
-
-            parent = parent.parent
-
-    def opponent_choice(self, action):
-
-        moves = [children.move for children in self.child]
-        ind_opponent_move = int(np.argwhere(moves == action))
-        child_opponent = self.child[ind_opponent_move]
-
-        return child_opponent
+        if winning:
+            parent.wins += 1
 
 
 def column_free(board, column):
--- agents/agent_Monte_Carlo/montecarlo_exec.py
+++ agents/agent_Monte_Carlo/montecarlo_exec.py
@@ -1,7 +1,7 @@
 import numpy as np
 import time
 from agents.common import BoardPiece, apply_player_action, PlayerAction
-from agents.agent_Monte_Carlo.montecarlo import TreeNode, change_player
+from agents.agent_Monte_Carlo.montecarlo import TreeNode, change_player, back_prop
 
 
 def montecarlo(board: np.ndarray, player: BoardPiece, saved_state, last_action: PlayerAction, train_time=5):
@@ -13,27 +13,32 @@ def montecarlo(board: np.ndarray, player: BoardPiece, saved_state, last_action: 
     if last_action is None:
         action = PlayerAction(3)
         apply_player_action(board, action, player, copy=False, pos=False)
-        root = TreeNode(board, 3, None, player)
+        root = TreeNode(board, action, None, player)
+        root.is_root = True
         node = root.select_node()
-        node.expansion_and_back_prop(player)
+        node.expansion(player)
         saved_state = root
 
     else:
         if saved_state is None:
             root = TreeNode(board, last_action, None, change_player(player))
+            root.is_root = True
 
         else:
             try:
                 root = saved_state.opponent_choice(last_action)
+                root.is_root = True
             except:
                 root = TreeNode(board, last_action, None, change_player(player))
+                root.is_root = True
 
         start = int(round(time.time()))
         present = int(round(time.time()))
 
         while (present - start) < train_time:
             node = root.select_node()
-            node.expansion_and_back_prop(player)
+            node, win = node.expansion(player)
+            back_prop(node, win)
             present = int(round(time.time()))
             num += 1
 
